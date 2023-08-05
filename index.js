d888df1e8c0f5dd1723a8b34d6755e4070cb4a8e
Rubaiyat Razin <rubaiyatraha12@gmail.com>
2023-08-06 00:13:26 +0600

    error check

--- index.js
+++ index.js
@@ -7,39 +7,46 @@ app.use(cors());
 
 const categories = require('./data/categories.json');
 const news = require('./data/news.json');
-const cors = require('cors');
+
 
 app.use(cors());
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('New API running')
-});
+try {
+    app.get('/', (req, res) => {
+        res.send('New API running')
+    });
 
-app.get('/news-categories', (req, res) => {
-    res.send(categories)
-});
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if (id === '08') {
+    app.get('/news-categories', (req, res) => {
+        res.send(categories)
+    });
+    app.get('/category/:id', (req, res) => {
+        const id = req.params.id;
+        if (id === '08') {
+            res.send(news);
+
+        }
+        else {
+            const category_news = news.filter(n => n.category_id === id)
+            res.send(category_news);
+
+        }
+
+    })
+    app.get('/news', (req, res) => {
         res.send(news);
-
-    }
-    else {
-        const category_news = news.filter(n => n.category_id === id)
-        res.send(category_news);
-        
-    }
+    })
+    app.get('/news/:id', (res, req) => {
+        const id = req.params.id;
+        const selectedNews = news.find(n => n._id === id);
+        res.send(selectedNews);
+    })
     
-})
-app.get('/news', (req, res) => {
-    res.send(news);
-})
-app.get('/news/:id', (res, req) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews);
-})
+} catch (error) {
+    
+}
+
+
 
 
 app.listen(port, () => {
