16732aa88e675ba416a26276d5c0abcbde17129e
Rubaiyat Razin <rubaiyatraha12@gmail.com>
2023-08-06 00:07:18 +0600

    last

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = process.env.port || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('New API running')
@@ -13,6 +18,27 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+});
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id)
+        res.send(category_news);
+        
+    }
+    
+})
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/news/:id', (res, req) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 
